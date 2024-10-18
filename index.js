a25e0c81de389b813bd6536ea63029cd10be76a8
verenaisele <77627438+verenaisele@users.noreply.github.com>
2024-10-18 13:44:13 +0200

    Layout anpassen

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
+
+Anweisungen:Generate all conversions, when the user clicks the button
+Round the numbers down to 3 decimal places
 */
